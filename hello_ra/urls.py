c9672a77e301ee9da15dd69e948dfd25041ba009
pkev@yandex.ru <pkev@yandex.ru>
2019-12-13 14:36:31 +0300

    add same urls

--- hello_ra/urls.py
+++ hello_ra/urls.py
@@ -13,9 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def hello(request):
+	return HttpResponse("Hello World")
+	
+def hello_python(request):
+	return HttpResponse("Hello python")
+	
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', hello),
+	url(r'^python/', hello_python),
+    url(r'^admin/', admin.site.urls),
+	
 ]
